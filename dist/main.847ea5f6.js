4640fd7a99777d617b02d802ad9c8a20f8314f2b
swissarmysam <s1804867@connect.glos.ac.uk>
2021-04-25 21:25:47 +0100

    Rebuilt for deployment

--- dist/main.847ea5f6.js
+++ dist/main.847ea5f6.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55902" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56696" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.847ea5f6.js.map
+//# sourceMappingURL=main.847ea5f6.js.map
--- dist/webroot.e31bb0bc.js
+++ dist/webroot.e31bb0bc.js
@@ -7421,7 +7421,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55902" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56696" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -7598,4 +7598,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/webroot.e31bb0bc.js.map
+//# sourceMappingURL=webroot.e31bb0bc.js.map
